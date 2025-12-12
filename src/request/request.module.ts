93a1d13e17a163dfe96beab3fcc14bc3609c53c4
bhavyaditpath <bhavyad.itpath@gmail.com>
2025-12-12 12:53:21 +0530

    Solve the dependency error

--- src/request/request.module.ts
+++ src/request/request.module.ts
@@ -5,9 +5,10 @@ import { RequestController } from './request.controller';
 import { Request } from './entities/request.entity';
 import { User } from '../user/entities/user.entity';
 import { Purchase } from '../purchase/entities/purchase.entity';
+import { AlertModule } from 'src/alert/alert.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Request, User, Purchase])],
+  imports: [TypeOrmModule.forFeature([Request, User, Purchase]), AlertModule],
   controllers: [RequestController],
   providers: [RequestService],
 })
